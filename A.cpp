56962a8c6be738bfb2010fce19eb33b6685fe46c
PrshntS <51121370+PrshntS@users.noreply.github.com>
2020-10-22 15:42:11 +0530

    .

--- A.cpp
+++ A.cpp
@@ -7,11 +7,21 @@ int main(){
 	freopen("output.txt","w",stdout);
 	#endif
 	
-	int t;
-	cin >> t;
-	while(t--){
-		
+	int n;
+	cin>>n;
+	int a[n];
+	for(int i=0;i<n;i++)
+	{
+		cin>>a[i];
 	}
+	int dp[n+1];
+	dp[0]=0;
+	dp[1]=abs(a[1]-a[0]);
+	for(int i=2;i<n;i++)
+	{
+		dp[i]=min(dp[i-1]+abs(a[i-1]-a[i]),dp[i-2]+abs(a[i-2]-a[i]));
+	}
+	cout<<dp[n-1];
 
 	return 0;
 }
