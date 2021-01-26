a0bd1cdb30c3bbb1ef118aae8fd97989a951c248
PrshntS <51121370+PrshntS@users.noreply.github.com>
2021-01-26 18:37:21 +0530

    Create fav.cpp

---
+++ fav.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main() {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+	ll t; cin >> t;
+	while (t--)
+	{
+		cout << "YES";
+
+	}
+
+
+	return 0;
+}
